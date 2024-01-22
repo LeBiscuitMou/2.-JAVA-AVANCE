2ece604473f7385ddc00092472374aa27c09dbd6
LeBiscuitMou <59147867+LeBiscuitMou@users.noreply.github.com>
2024-01-23 00:15:18 +0100

    aa

--- 2. TP/fistJavaFX/src/main/java/net/ent/etrs/fistJavaFX/start/Lanceur.java
+++ 2. TP/fistJavaFX/src/main/java/net/ent/etrs/fistJavaFX/start/Lanceur.java
@@ -1,16 +1,9 @@
 package net.ent.etrs.fistJavaFX.start;
 
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
